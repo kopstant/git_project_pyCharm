4215479b5c2a896086810ef33c47b0f38025f169
Dmitriy Nadelyaev <kopstant99@yandex.ru>
2024-02-17 21:56:57 +0900

    replace correction function for + and -

--- funcs.py
+++ funcs.py
@@ -1,13 +1,10 @@
 def add(a,b):
-  return a+b
+  print (a+b)
 
 def minus(a,b):
-  return a-b
+  print (a-b)
 
 
-a = int(input())
-b = int(input())
-how_many_plus = add(a,b)
-how_many_minus = minus(a,b)
+
 
 
