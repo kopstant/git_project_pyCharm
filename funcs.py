2660f93de261b2658cd321d057a15f6622c5a0db
Dmitriy Nadelyaev <kopstant99@yandex.ru>
2024-02-17 21:16:53 +0900

    append function a-b

--- funcs.py
+++ funcs.py
@@ -1,8 +1,13 @@
 def add(a,b):
   return a+b
 
+def minus(a,b):
+  return a-b
+
 
 a = int(input())
 b = int(input())
-how_many = add(a,b)
-print (how_many)
+how_many_plus = add(a,b)
+how_many_minus = minus(a,b)
+print (f"Результат сложения {how_many_plus}")
+print (f"Результат вычитания {how_many_minus}")
